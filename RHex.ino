6e45282a956ee4b8796ff3b56f4ff1d95b750e72
Daniel Armyr <daniel.armyr@frescano.se>
2020-07-09 00:02:32 +0200

    Added the PID library as well.

--- RHex.ino
+++ RHex.ino
@@ -1,11 +1,14 @@
-//TODO: Use PID library here: https://github.com/br3ttb/Arduino-PID-Library/
+//Dependency: https://github.com/PaulStoffregen/Encoder/commit/4c4ec3ad970651324e0c197b179a682e9604ab08
+//DEpendency: https://github.com/br3ttb/Arduino-PID-Library/commit/9b4ca0e5b6d7bab9c6ac023e249d6af2446d99bb
 
 #include "Arduino.h"
 #include <Encoder.h>
+#include <PID_v1.h>
 
 #include "MotorDriver.h"
 #include "SerialEchoBeacon.h"
 #include "TaskScheduler.h"
+
 
 SerialEchoBeacon beacon(1000);
 const int NUM_TASKS = 1;
